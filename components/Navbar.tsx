f5f39b07b9eadbc187763adc760c080eb435f6e6
Mahdi-Shayan <mahdieshayan4030@gmail.com>
2025-03-26 06:44:27 -0700

    create Header UI

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,25 @@
-
+import { avatarImages } from "@/constanst";
+import Image from "next/image";
 
 function Navbar() {
+  const user: string = avatarImages[0];
+
   return (
-    <div>Navbar</div>
-  )
+    <header className="bg-dark-300 h-[70px]">
+      <nav className="flex gap-5 justify-end items-center h-full w-full px-15">
+        <p className="relative hover:text-red-400 cursor-pointer transition-all logout-after">
+          logout
+        </p>
+        <Image
+          src={user}
+          alt="user porofile"
+          width={40}
+          height={40}
+          className="rounded-full"
+        />
+      </nav>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
