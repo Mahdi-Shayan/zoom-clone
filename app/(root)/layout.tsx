2ab6d653c5068411ba19a0215997bc9bed301153
Mahdi-Shayan <mahdieshayan4030@gmail.com>
2025-03-30 07:49:22 -0700

    set up stream

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,16 @@
+import { Toaster } from "@/components/ui/sonner";
+import StreamVideoProvider from "@/providers/streamCloneProvider";
 import { ReactNode } from "react";
 
 function RootLayout({ children }: { children: ReactNode }) {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <StreamVideoProvider>
+        {children}
+      </StreamVideoProvider>
+      <Toaster richColors/>
+    </main>
+  );
 }
 
 export default RootLayout;
